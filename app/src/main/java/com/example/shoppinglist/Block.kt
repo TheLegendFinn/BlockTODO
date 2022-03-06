6fee3009e41ce7972574c5d125e845064995a1d5
TheLegendFinn <74185331+TheLegendFinn@users.noreply.github.com>
2022-03-06 19:33:59 +0100

    Added Comments and Class Documentation

--- app/src/main/java/com/example/shoppinglist/Block.kt
+++ app/src/main/java/com/example/shoppinglist/Block.kt
@@ -1,5 +1,10 @@
 package com.example.shoppinglist
 
-class Block (val title: String, val items: ArrayList<String>) {
+/**
+ * Describes the components of the Blocks in the Main Acticity
+ * @param title Title of the Block
+ * @param items List of the list items
+ */
 
+class Block (val title: String, val items: ArrayList<String>) {
 }
--- app/src/main/java/com/example/shoppinglist/adapters/BlockAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/adapters/BlockAdapter.kt
@@ -10,8 +10,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.Block
 import com.example.shoppinglist.R
 
+/**
+ * Class connects the [Block]s with the RecyclerView of the MainActivity
+ * @param context Activity Context
+ * @param blocks List of Blocks to process
+ */
 class BlockAdapter(val context: Context, val blocks: ArrayList<Block>) :
     RecyclerView.Adapter<BlockAdapter.ViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             LayoutInflater.from(context).inflate(R.layout.recycler_block, parent, false)
@@ -19,14 +25,18 @@ class BlockAdapter(val context: Context, val blocks: ArrayList<Block>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        //Get the block and set Title
         val block = blocks.get(position)
-        holder.textView.text = block.title
+        holder.blockName.text = block.title
 
+        //LayoutInflater to inflate the Block Items
         val inflater =  context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
         for (item in block.items) {
+            //Inflate the Item-Layout
             val tv = inflater.inflate(R.layout.block_item, null)
 
+            //Set Item-Text and attach to the LinearLayout
             tv.findViewById<TextView>(R.id.block_item_name).setText(item)
             holder.linearLayout.addView(tv)
         }
@@ -36,8 +46,14 @@ class BlockAdapter(val context: Context, val blocks: ArrayList<Block>) :
         return blocks.size
     }
 
+    /**
+     * Subclass that defines the ViewHolder for the Adapter
+     * @param view View inside the ViewHolder
+     * @property blockName Name of the Block
+     * @property linearLayout LinearLayout to add the items into
+     */
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView = view.findViewById<TextView>(R.id.recycler_block_name)
+        val blockName = view.findViewById<TextView>(R.id.recycler_block_name)
         val linearLayout = view.findViewById<LinearLayout>(R.id.recycler_block_lin)
     }
 }
